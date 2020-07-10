dff177c3ad5ba5a170ce43964f9987eac6cbd6a0
andy580 <andy.gehlan@gmail.com>
2020-07-10 13:51:32 -0400

    Added function to print map to console

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@
 using std::cout;
 using std::vector;
 
+void printMap(vector<vector<int>> &map) {
+    for (auto i = map.begin(); i != map.end(); i++) {
+        for (auto j = i->begin(); j != i->end(); j++) {
+            cout << *j << " ";
+        }
+        cout << "\n";
+    }
+};
+
 int main () {
-    vector<vector<int>> board = {
+    vector<vector<int>> map = {
         {0,1,0,0,0,0},
         {0,1,0,0,0,0},
         {0,1,0,0,0,0},
@@ -13,4 +22,8 @@ int main () {
         {0,0,0,1,0,0},
         {0,1,0,1,1,0}          
     };
+
+    void printMap(vector<vector<int>> &map); 
+    printMap(map);
+
 }
